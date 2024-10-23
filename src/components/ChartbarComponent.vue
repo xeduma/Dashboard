9793d5063e6bfe08085451646750b2969ad1f6d7
xeduma <82473152+xeduma@users.noreply.github.com>
2024-10-23 16:43:42 +0200

    Create ChartbarComponent.vue

---
+++ src/components/ChartbarComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h3>Graphique à Barres</h3>
+    <apexchart type="bar" :options="chartOptions" :series="chartSeries"></apexchart>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+  name: "ChartBarComponent",
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: "bar",
+        },
+        labels: ["Label 1", "Label 2", "Label 3", "Label 4"],
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 300,
+              },
+              legend: {
+                position: "bottom",
+              },
+            },
+          },
+        ],
+        title: {
+          text: "Données Barres",
+          align: "center",
+        },
+      },
+      chartSeries: [
+        {
+          name: "Series 1",
+          data: [30, 40, 45, 50],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  text-align: center;
+  color: #333;
+}
+
+.apexcharts-canvas {
+  margin: 0 auto;
+  max-width: 100%;
+  width: 400px;
+  height: auto;
+}
+
+@media (max-width: 480px) {
+  .apexcharts-canvas {
+    width: 300px;
+  }
+}
+</style>
