752206c6abacdafa74d75421a978a8d283ba0c34
xeduma <82473152+xeduma@users.noreply.github.com>
2024-10-23 16:41:36 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+ <div id="app">
+
+<!-- 1 div avec le graph ligne -->
+    <div id="chartcomponent">
+        <h1>apexchart2</h1>
+        <ChartComponent />
+    </div>
+
+<!-- 2 div avec le graphique donut -->
+    <div id="chartdonuts">
+      <ChartdonutsComponent />
+    </div>
+
+<!-- 3 div avec le graphique barre colones -->
+    <div id="chartbar">
+      <ChartbarComponent />
+    </div>
+
+
+ </div>
+</template>
+
+<script>
+import ChartComponent from './components/ChartComponent.vue';
+import ChartdonutsComponent from './components/ChartdonutsComponent.vue';
+import ChartbarComponent from './components/ChartbarComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ChartComponent,
+    ChartdonutsComponent,
+    ChartbarComponent,
+  }
+};
+</script>
+
+<style>
+/* Add global styles here */
+</style>
